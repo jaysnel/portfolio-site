ab8e3587e5a31a815122717980d1883190ea30dc
jaylan <jaylan210@gmail.com>
2020-11-21 22:28:43 -0500

    completley over particle js, its a dumb framework. Just adding vue router.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="app">
-    <HomeHero/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HomeHero from './components/HomeHero.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HomeHero
-  }
-}
-</script>
-
 <style>
 #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HomeHero />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomeHero from '@/components/HomePage/HomeHero.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HomeHero
+  }
+}
+</script>
