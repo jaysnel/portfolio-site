f3341d276195cadb7ff6b56cb36ca7e2fec4d4ad
jaylan <jaylan210@gmail.com>
2020-11-22 19:17:00 -0500

    adding fontawesome with vue wasnt so bad, this way should keep file size down a agood bit.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
+      <router-link to="/"><font-awesome-icon icon="home" />Home</router-link>
+      <router-link to="/about"><font-awesome-icon icon="user" />About</router-link>
     </div>
     <router-view/>
     <vue-particles 
         color="#dedede"
         :particleOpacity="1"
-        :particlesNumber="200"
+        :particlesNumber="300"
         shapeType="circle"
         :particleSize="4"
         linesColor="#dedede"
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueParticles from 'vue-particles'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faHome, faUser)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(VueParticles)
 
