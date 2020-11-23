57b440a4e6d1d5baf1585e78e089a257ed8b7e50
jaylan <jaylan210@gmail.com>
2020-11-22 19:09:13 -0500

    adding sass, start of updating nav. Also saving before adding fontawesome. Going to try different ways due to file size.

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,22 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    <vue-particles 
+        color="#dedede"
+        :particleOpacity="1"
+        :particlesNumber="200"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#dedede"
+        :linesWidth="1">
+        </vue-particles>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  position: sticky;
-  padding: 30px;
-  z-index: 999;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import './assets/css/style.css';
 </style>
--- src/components/HomePage/HomeHero.vue
+++ src/components/HomePage/HomeHero.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="HomeHero">
     <div class="title">Hi, I'm <span>Jaylan Snelson</span>, Web Developer</div>
-    <vue-particles 
-        color="#dedede"
-        :particleOpacity="1"
-        :particlesNumber="200"
-        shapeType="circle"
-        :particleSize="4"
-        linesColor="#dedede"
-        :linesWidth="1">
-        </vue-particles>
   </div>
 </template>
 
@@ -24,13 +15,4 @@ export default {
 .title {
   margin-top: 15px;
 }
-
-  #particles-js {
-    background-size: cover;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
 </style>
