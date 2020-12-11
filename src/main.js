1f55bc4f6dab9aae417b9f2bd6ef6324397c9840
jaylan <jaylan210@gmail.com>
2020-12-10 19:26:17 -0500

    adding icon, guess i forgot to save

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import router from './router'
 import VueParticles from 'vue-particles'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faHome, faUser, faTools,  faIdCard, faBriefcase} from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faTools,  faIdCard, faBriefcase, faThList} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faHome, faUser, faTools, faIdCard, faBriefcase, faTwitter, faLinkedin, faGithub)
+library.add(faHome, faUser, faTools, faIdCard, faBriefcase, faThList, faTwitter, faLinkedin, faGithub)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(VueParticles)
