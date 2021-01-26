891f464cd80eb4e7ad14dc3ec85dd6cee3f3ac47
root <jaylan210@gmail.com>
2021-01-25 20:49:19 -0500

    saving here before adding server for contact form emails

--- src/components/ContactPage/Contact.vue
+++ src/components/ContactPage/Contact.vue
@@ -3,21 +3,49 @@
       <h1 class="page-title contact-title">Contact Me</h1>
       <form class="form-contact-message" action="" method="post" name="form">
           <label for="Name">Name:</label>
-          <input class="form-styling" type="text" name="Name" placeholder=""/>
+          <input class="form-styling" type="text" name="Name" placeholder="Name" v-model="name"/>
 
           <label for="Email">Email:</label>
-          <input class="form-styling" type="text" name="Email" placeholder=""/>
+          <input class="form-styling" type="text" name="Email" placeholder="Email" v-model="email"/>
 
-          <label for="Email">Message:</label>
-          <textarea class="form-styling" type="text" name="Message" placeholder=""/>
+          <label for="Message">Message:</label>
+          <textarea class="form-styling" type="text" name="Message" placeholder="Message" v-model="message"/>
 
-          <button>Send Message</button>
+          <button @click="sendEmail()">Send Message</button>
 				</form>
   </div>
 </template>
 
 <script>
+
 export default {
-name: "Contact"
+name: "Contact",
+data() {
+  return {
+    name: "",
+      email: "",
+      message: ""
+  }
+},
+  methods: {
+    // sendEmail(e) {
+    //   try {
+    //     console.log("trying...");
+    //     emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target,
+    //     'YOUR_USER_ID', {
+    //       name: this.name,
+    //       email: this.email,
+    //       message: this.message
+    //     })
+    //   console.log("finished")
+    //   } catch(error) {
+    //       console.log({error})
+    //   }
+    //   // Reset form field
+    //   this.name = ''
+    //   this.email = ''
+    //   this.message = ''
+    // }
+  }
 }
 </script>
