24e9c6d35a61ff0cc4b381191a4505068c784470
root <jaylan210@gmail.com>
2021-03-10 20:48:37 -0500

    adding connect-history-api-fallback to fix routing, serving from serve.js now

--- serve.js
+++ serve.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const serveStatic = require("serve-static")
 const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 8080;
-app.listen(port); 
+const history = require('connect-history-api-fallback');
+
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname, '/dist'))
+
+app.use(staticFileMiddleware)
+app.use(history())
+app.use(staticFileMiddleware)
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname, '/dist/index.html'))
+})
+
+app.listen(port, function () {
+  console.log( `runing on port ${port}` )
+})
