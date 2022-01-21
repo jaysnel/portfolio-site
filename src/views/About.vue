09c871daba968c3295d11d0e6585a3f136b18819
Jaylan Snelson <jaylan210@gmail.com>
2022-01-21 14:50:33 -0500

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,15 +4,14 @@
     <h1 class="page-title about-title">About Me</h1>
     <div class="about-me-description">
       <p>
-        My name is Jaylan Snelson and I'm a 26 year old Front End Developer based
-        wherever I can find work. :)
+        My name is Jaylan Snelson and I'm a 28 year old Front End Engineer based
+        wherever I can find work!
       </p>
       <p>
         I describe myself as a passionate developer on a lifelong learning path.
       </p>
       <p>
-        In my free time you can find me playing chess, learning about security, excercising or
-        just relaxing around the house with the dog.
+        In my free time you can find me playing chess, training in Brazilian jiu jitsu or just relaxing around the house.
       </p>
       <p>
         This also isn't the only place on the web where you can find me, here is a few more along with my work history. Talk to you soon!
